183c1ab7bcd48a6f0a828ff7443382dd66a84770
Divy Yadav <divyyadav@Divys-MacBook-Air.local>
2025-09-29 23:19:41 +0530

    added datasets

---
+++ Scripts/Untitled-1.sql
@@ -0,0 +1,42 @@
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates a new database named 'DataWarehouse' after checking if it already exists. 
+    If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
+    within the database: 'bronze', 'silver', and 'gold'.
+	
+WARNING:
+    Running this script will drop the entire 'DataWarehouse' database if it exists. 
+    All data in the database will be permanently deleted. Proceed with caution 
+    and ensure you have proper backups before running this script.
+*/
+
+
+
+
+USE master;
+
+-- Drop and recreate the 'DataWarehouse' database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
+END;
+GO
+
+CREATE DATABASE DataWarehouseDB;
+
+USE DataWarehouseDB;
+
+IF SCHEMA_ID('bronze') IS NULL
+    EXEC('CREATE SCHEMA bronze');
+GO
+
+
+IF SCHEMA_ID('silver') IS NULL
+    EXEC('CREATE SCHEMA silver');
+
+IF SCHEMA_ID('gold') IS NULL
+    EXEC('CREATE SCHEMA gold');
